0fc88125f1d03d04b2bc4e2d226ec928ef53eedc
Mukesh <66948644+mukesh61112@users.noreply.github.com>
2026-02-20 14:57:14 +0530

    Update macros.h

--- HFT/Engine/Common/macros.h
+++ HFT/Engine/Common/macros.h
@@ -6,7 +6,8 @@
 /// Branch prediction hints.
 #define LIKELY(x) __builtin_expect(!!(x), 1)
 #define UNLIKELY(x) __builtin_expect(!!(x), 0)
-
+//auto only with above version on c++ else use
+//inline void ASSERT(bool cond , const std::string &msg)
 /// Check condition and exit if not true.
 inline auto ASSERT(bool cond, const std::string &msg) noexcept {
   if (UNLIKELY(!cond)) {
