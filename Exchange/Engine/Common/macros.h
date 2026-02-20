74ca4780d2d06a0753281aad2b9394efed5c5ac1
Mukesh <66948644+mukesh61112@users.noreply.github.com>
2026-02-20 11:54:35 +0530

    Add branch prediction macros and error handling
    Added branch prediction macros and improved error handling.

--- Exchange/Engine/Common/macros.h
+++ Exchange/Engine/Common/macros.h
@@ -4,8 +4,11 @@
 #include <iostream>
 
 /// Branch prediction hints.
+
+
 #define LIKELY(x) __builtin_expect(!!(x), 1)
 #define UNLIKELY(x) __builtin_expect(!!(x), 0)
+
 
 /// Check condition and exit if not true.
 inline auto ASSERT(bool cond, const std::string &msg) noexcept {
@@ -15,6 +18,7 @@ inline auto ASSERT(bool cond, const std::string &msg) noexcept {
     exit(EXIT_FAILURE);
   }
 }
+
 
 inline auto FATAL(const std::string &msg) noexcept {
   std::cerr << "FATAL : " << msg << std::endl;
